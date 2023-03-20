af63ac6b363f7b0d0b3f74a4d293d15af609a6b0
hai0594 <haind290594@gmail.com>
2023-03-20 15:49:39 +0700

    day 11

---
+++ day 11/bt1.py
@@ -0,0 +1,24 @@
+# Add function
+def add_number(x,y):
+    add_func = x + y
+    return add_func
+
+print(f"Add amount:{add_number(5,10)}")
+
+# Sub function
+def sub_number(x,y):
+    sub_func = x - y
+    return sub_func
+print(f"Sub amount:{sub_number(10,7)}")
+
+# Multi function
+def multi_number(x,y):
+    multi_func = x*y
+    return multi_func
+print(f"Multi amount:{multi_number(2,4)}")
+
+# Divi function
+def divi_number(x,y):
+    divi = x/y
+    return divi
+print(f"Divi amount:{divi_number(10,5)}")
