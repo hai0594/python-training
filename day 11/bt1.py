18d6e85c9d3815cdaeabd8de7881ae7fcbed6f8b
hai0594 <haind290594@gmail.com>
2023-03-20 15:59:15 +0700

    update

--- day 11/bt1.py
+++ day 11/bt1.py
@@ -1,24 +1,20 @@
 # Add function
 def add_number(x,y):
-    add_func = x + y
-    return add_func
+    return x + y
 
 print(f"Add amount:{add_number(5,10)}")
 
 # Sub function
 def sub_number(x,y):
-    sub_func = x - y
-    return sub_func
+    return x - y
 print(f"Sub amount:{sub_number(10,7)}")
 
 # Multi function
 def multi_number(x,y):
-    multi_func = x*y
-    return multi_func
+    return x*y
 print(f"Multi amount:{multi_number(2,4)}")
 
 # Divi function
 def divi_number(x,y):
-    divi = x/y
-    return divi
+    return x/y
 print(f"Divi amount:{divi_number(10,5)}")
